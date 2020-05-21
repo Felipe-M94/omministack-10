63ddd390e2ce1fccb527161a157575005b14da7c
Felipe <felipemendonca@stitdata.com>
2020-05-21 14:54:29 -0300

    conclusion-web

--- web/src/App.js
+++ web/src/App.js
@@ -1,45 +1,49 @@
-import React from "react";
+import React, { useState, useEffect } from "react"
+import api from "./services/api"
 
-import "./global.css";
-import "./App.css";
+import "./global.css"
+import "./App.css"
 import "./Sidebar.css"
+import "./Main.css"
+
+import DevItem from './components/Devitem'
+import DevForm from './components/DevForm'
 
 function App() {
+  const [devs, setDevs] = useState([])
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs")
+
+      setDevs(response.data)
+    }
+
+    loadDevs()
+  }, []);
+
+  async function handlerAddDev(data) {
+    const response = api.post("/devs", data)
+
+    setDevs([...devs, response.data])
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastro</strong>
-        <form>
-          <div class="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div class="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div class="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div class="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-           </div> 
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handlerAddDev} />
       </aside>
+      
       <main>
-
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
       </main>
     </div>
   );
 }
 
-export default App;
+export default App
